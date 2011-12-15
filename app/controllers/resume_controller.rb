84e3c671da850d9973199c80400c3090400f119e
Justin Campbell <justin@justincampbell.me>
2011-12-15 11:20:21 -0500

    Use Resume from GitHub Gist

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,13 +1,13 @@
 class ResumeController < ApplicationController
   layout nil
-  
+
   def index
-    @md = File.read('public/resume.md')
-    
+    @md = File.read('public/resume/JustinCampbell.md')
+
     respond_to do |format|
       format.html
       # format.md # Serve markdown directly from /public
     end
   end
-  
+
 end
