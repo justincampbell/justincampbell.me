bf4e245d109ef6da11bc08a1917d4f43ad440a29
Justin Campbell <justin@justincampbell.me>
2011-12-19 11:32:06 -0500

    Add Resume tests

--- test/functional/resume_controller_test.rb
+++ test/functional/resume_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ResumeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context "html" do
+    should "be accessible" do
+      get :index, :format => :html
+      assert_response :success
+    end
+  end
+
+  context "markdown" do
+    should "be accessible via .md" do
+      get :index, :format => :md
+      assert_response :success
+    end
+
+    should "be accessible via .markdown" do
+      get :index, :format => :markdown
+      assert_response :success
+    end
+  end
+
+  context "plaintext" do
+    should "be accessible via .txt" do
+      get :index, :format => :txt
+      assert_response :success
+    end
+
+    should "be accessible via .text" do
+      get :index, :format => :text
+      assert_response :success
+    end
+  end
 end
