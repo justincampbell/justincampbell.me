35e768fd54f95e5c7a1249a3f6afc4a2926a1d52
Justin Campbell <justin@justincampbell.me>
2011-12-20 22:03:17 -0500

    Add route for Tumblr post paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Justincampbell::Application.routes.draw do
   root :to => 'pages#index'
-  
+
   get 'blog'       => 'blog#index'
   get 'blog/:slug' => 'blog#show'
-  
+
+  # Handle legacy Tumblr URLs
+  get 'post/:tumblr_id/:slug' => 'blog#show'
+
   get 'resume' => 'resume#index'
-  
+
   namespace :admin do
     root :to => 'admin#index'
     resources :posts
