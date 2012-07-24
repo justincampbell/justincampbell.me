48ec0a9007687068d21e42a4d199f9ef4b54d3da
Justin Campbell <justin@justincampbell.me>
2012-07-24 10:11:09 -0400

    Add image_url to Project model, update views

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :name, :published_at, :url
+  attr_accessible :image_url, :name, :published_at, :url
 
   default_scope do
     where("published_at is not null").order("published_at desc")
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120710120446) do
+ActiveRecord::Schema.define(:version => 20120723222627) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120710120446) do
     t.datetime "published_at"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "image_url"
   end
 
 end
