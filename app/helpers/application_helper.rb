f6b587c602379635b58fd8308cff3580b05bbfd4
Justin Campbell <justin@justincampbell.me>
2012-11-29 08:38:38 -0500

    Do not link twitter usernames in code blocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
+  TWITTER_URL_PREFIX = "https://twitter.com/intent/user?screen_name="
+
   def markdown(md, options={})
     redcarpet = Redcarpet::Markdown.new MarkdownRenderer,
       autolink: true,
       fenced_code_blocks: true
 
-    md.gsub! /@(\w+)/, "[@\\1](https://twitter.com/intent/user?screen_name=\\1)"
+    md = link_twitter_usernames(md)
 
     html = redcarpet.render md
 
@@ -17,5 +19,19 @@ module ApplicationHelper
     html = options[:preview] ? html.split(more)[0] : html.gsub(more, "")
 
     raw html
+  end
+
+  private
+
+  def link_twitter_usernames(markdown)
+    code_block = false
+
+    result = markdown.split("").inject("") { |buffer, letter|
+      code_block = !code_block if letter == "`"
+      letter.prepend "twitter:" if not code_block and letter == "@"
+      buffer << letter
+    }
+
+    result.gsub /twitter:@(\w+)/, "[@\\1](#{TWITTER_URL_PREFIX}\\1)"
   end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+
+describe ApplicationHelper do
+  describe "#markdown" do
+    subject { helper.markdown(markdown).strip }
+
+    let(:markdown) { "Body" }
+
+    it "renders to html" do
+      should == "<p>Body</p>"
+    end
+
+    context "with an h1" do
+      let(:markdown) { "# Header" }
+
+      it "adds 3 levels" do
+        should == "<h4>Header</h4>"
+      end
+    end
+
+    context "with a twitter username" do
+      let(:markdown) { "@justincampbell" }
+
+      it "linkifies it" do
+        should include(
+          "<a href=\"#{klass::TWITTER_URL_PREFIX}justincampbell\">@justincampbell</a>"
+        )
+      end
+    end
+
+    context "with an instance variable in code" do
+      let(:markdown) { "`@ivar`\n\n```rb\n@ivar\n```" }
+
+      it "does not convert it to a twitter link" do
+        should_not include(
+          "#{klass::TWITTER_URL_PREFIX}ivar"
+        )
+      end
+    end
+  end
+end
