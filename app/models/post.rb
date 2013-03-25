1e07c6e00647814a47199972a0176eade2f31952
Justin Campbell <justin@justincampbell.me>
2013-03-25 11:38:02 -0400

    Remove trailing whitespace

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,16 @@
 class Post < ActiveRecord::Base
   before_save :update_or_create_slug
-  
+
   validates_uniqueness_of :title, :slug
-  
+
   def permalink
     "/blog/#{self.to_param}"
   end
-  
+
   def to_param
     slug or title.parameterize
   end
-  
+
   def update_or_create_slug
     self.slug = self.title.try(:parameterize)
   end
