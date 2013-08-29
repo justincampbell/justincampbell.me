9b6c58230af599b24165aef9550b9caea4845e01
Justin Campbell <justin@justincampbell.me>
2013-08-29 10:43:18 -0400

    Store secret token in an environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Justincampbell::Application.config.secret_token = '0a861066a1b56e82b00faba6edab42156d6ce57ed185db3ffdc26c7e687300fc869823e56eabb33652e1205e2fe11eb5130507d6e131e2274691b59693e4ee9e'
+Justincampbell::Application.config.secret_token = ENV['SECRET_TOKEN'] || SecureRandom.hex(128)
