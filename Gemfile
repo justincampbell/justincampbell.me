59d754e49bec7521ff535c4c16b10b2933eaf0c6
Justin Campbell <justin@justincampbell.me>
2015-04-24 20:56:20 -0400

    Add Google Analytics

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 gem 'middleman'
 
 gem 'bourbon'
+gem 'middleman-google-analytics'
 gem 'middleman-livereload'
 gem 'slim'
--- config.rb
+++ config.rb
@@ -1,6 +1,10 @@
 set :css_dir, 'stylesheets'
 set :images_dir, 'images'
 
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-5973800-7'
+end
+
 configure :build do
   activate :asset_hash
   activate :minify_css
