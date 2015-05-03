3d94d73751fe6934079990cbfebb52b087e34f0e
Justin Campbell <justin@justincampbell.me>
2015-05-02 20:13:17 -0400

    Add bucket policy

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,30 @@
 provider "aws" { region = "us-east-1" }
 
+variable "bucket" {
+  default = "justincampbell.me"
+}
+
 resource "dnsimple_record" "apex" {
-  domain = "justincampbell.me"
+  domain = "${var.bucket}"
   name = ""
   type = "ALIAS"
   value = "${aws_s3_bucket.default.website_endpoint}"
 }
 
 resource "aws_s3_bucket" "default" {
-  bucket = "justincampbell.me"
+  bucket = "${var.bucket}"
   acl = "public-read"
+  policy = "${template_file.policy.rendered}"
 
   website {
     index_document = "index.html"
+  }
+}
+
+resource "template_file" "policy" {
+  filename = "policy.json"
+
+  vars {
+    bucket = "${var.bucket}"
   }
 }
